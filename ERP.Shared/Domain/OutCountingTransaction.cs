f464d0675df37279985b4f3c7ff9ebd7c8c3b7b8
Berkan Serbes <berkan_serbes@hotmail.com>
2025-02-08 20:15:40 +0300

    Add new domain classes in ERP.Shared.Domain namespace
    Introduced new classes with basic structure and necessary using directives:
    - ConsumableTransaction, InCountingTransaction, OutCountingTransaction, ProductionTransaction (inherit from BaseProductTransaction)
    - PurchaseOrder, PurchaseOrderLine, SalesOrder, SalesOrderLine, SubUnitset, TransferTransaction, Unitset, WastageTransaction (no inheritance)
    - Variant (inherits from BaseEntity)

---
+++ ERP.Shared/Domain/OutCountingTransaction.cs
@@ -0,0 +1,12 @@
+﻿using ERP.Shared.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Shared.Domain;
+
+public class OutCountingTransaction : BaseProductTransaction
+{
+}
