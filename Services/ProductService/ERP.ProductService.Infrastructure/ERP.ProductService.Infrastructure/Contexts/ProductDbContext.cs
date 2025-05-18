e8b57d6a24bc06d8b376e97ea30100346e951596
Berkan Serbes <berkan_serbes@hotmail.com>
2025-05-18 12:08:37 +0300

    Refactor ProductDbContext for clarity and configuration
    Reformatted `ProductDbContext.cs` to improve code structure.
    Moved `using` directives to the top, added a constructor to
    accept `DbContextOptions`, and implemented the
    `OnModelCreating` method to apply configurations from the
    assembly, enhancing the context's configuration capabilities.

--- Services/ProductService/ERP.ProductService.Infrastructure/ERP.ProductService.Infrastructure/Contexts/ProductDbContext.cs
+++ Services/ProductService/ERP.ProductService.Infrastructure/ERP.ProductService.Infrastructure/Contexts/ProductDbContext.cs
@@ -4,8 +4,17 @@ using Microsoft.EntityFrameworkCore;
 namespace ERP.ProductService.Infrastructure.Contexts;
 
 public class ProductDbContext : DbContext
-{
-	public DbSet<Product> Products { get; set; }
+{	public DbSet<Product> Products { get; set; }
 	public DbSet<Variant> Variants { get; set; }
-	public DbSet<Warehouse> Warehouses { get; set; }
+	public DbSet<Warehouse> Warehouses { get; set; }	public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductDbContext).Assembly);
+
+		base.OnModelCreating(modelBuilder);
+	}
+
 }
