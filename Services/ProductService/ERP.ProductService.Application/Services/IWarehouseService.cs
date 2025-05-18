8d02bd8b6c0bd11a409f56fee1597602bfb193f3
Berkan Serbes <berkan_serbes@hotmail.com>
2025-05-18 11:38:27 +0300

    Add GetWarehouseRequest and update IWarehouseService
    Introduces the `GetWarehouseRequest` class with a `Number` property. Updates `IWarehouseService` with new using directives and adds asynchronous methods for retrieving, adding, updating, and deleting warehouse data, utilizing `GetWarehouseResponse` and `CreateWarehouseRequest`.

---
+++ Services/ProductService/ERP.ProductService.Application/Services/IWarehouseService.cs
@@ -0,0 +1,15 @@
+﻿using ERP.Shared.Contracts.DTOs.ProductService.Warehouse.Requests;
+using ERP.Shared.Contracts.DTOs.ProductService.Warehouse.Responses;
+using ERP.Shared.Contracts.Results;
+using ERP.Shared.Domain.Entities;
+
+namespace ERP.ProductService.Application.Services;
+
+public interface IWarehouseService
+{
+	Task<DataResult<IEnumerable<GetWarehouseResponse>>> GetAllAsync();
+	Task<DataResult<GetWarehouseResponse>> GetByNumberAsync(CreateWarehouseRequest createWarehouseRequest);
+	Task<DataResult<CreateWarehouseResponse>> AddAsync(CreateWarehouseRequest createWarehouseRequest);
+	Task<DataResult<UpdateWarehouseResponse>> UpdateAsync(UpdateWarehouseRequest updateWarehouseRequest);
+	Task<DataResult<DeleteWarehouseResponse>> DeleteAsync(DeleteWarehouseRequest deleteWarehouseRequest);
+}
